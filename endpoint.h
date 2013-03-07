fe52e75f91ae312f679941ccb31920ce8e5c9e06
yongboy <yongboy@gmail.com>
2013-03-07 13:57:32 +0800

    remove the endpoint's implemention code

--- endpoint.h
+++ endpoint.h
@@ -1,24 +1,25 @@
 #ifndef _ENDPOINT_H
 #define _ENDPOINT_H
 
-//[message type] ':' [message id ('+')] ':' [message endpoint] (':' [message data]) 
-typedef struct{
-  char *message_type;
-  char *message_id;
-  char *endpoint;
-  char *message_data;
-  char *ori_data;
+//[message type] ':' [message id ('+')] ':' [message endpoint] (':' [message data])
+typedef struct {
+    char *message_type;
+    char *message_id;
+    char *endpoint;
+    char *message_data;
+    char *ori_data;
 } message_fields;
 
 typedef struct {
     char *endpoint;
     void (*on_init)(const char *endpoint);
-    void (*on_connect)(const char *sessionid, const message_fields *msg_fields);
+    void (*on_connect)(const char *sessionid);
     void (*on_message)(const char *sessionid, const message_fields *msg_fields);
+    void (*on_json_message)(const char *sessionid, const message_fields *msg_fields);
+    void (*on_event)(const char *sessionid, const message_fields *msg_fields);
+    void (*on_other)(const char *sessionid, const message_fields *msg_fields);
     void (*on_disconnect)(const char *sessionid, const message_fields *msg_fields);
     void (*on_destroy)(const char *endpoint);
 } endpoint_implement;
-
-char *format_message(message_fields *msg_fields);
 
 #endif

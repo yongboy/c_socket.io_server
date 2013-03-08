c18285b0f68cfc7e9339c6b1b94c1b4e93c2f6b5
yongboy <yongboy@gmail.com>
2013-03-08 16:06:46 +0800

    Register the /whiteboard

--- example/socket.io_server.c
+++ example/socket.io_server.c
@@ -4,6 +4,7 @@ int main(int argc, char const *argv[]) {
     server_init();
 
     server_register_endpoint(init_chat_endpoint_implement("/chat"));
+    server_register_endpoint(init_whiteboard_endpoint_implement("/whiteboard"));
     
     int port = 8000;
     server_run(port);

4698798a71accd40a2a4518251a298010c6084c2
yongboy <yongboy@gmail.com>
2013-03-12 15:38:21 +0800

    For safe free pointer

---
+++ safe_mem.h
@@ -0,0 +1,29 @@
+#ifndef SAFE_MEM_H
+#define SAFE_MEM_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static void *debug_malloc(size_t size, const char *file, int line, const char *func) {
+    void *p;
+    p = malloc(size);
+    // printf("%s:%d:%s:malloc(%ld): p=0x%lx\n",
+    //        file, line, func, size, (unsigned long)p);
+    if(p == NULL){
+    	fprintf(stderr, "malloc apply size %ld failure!\n", size);
+    }
+    
+    return p;
+}
+
+#define malloc(s) debug_malloc(s, __FILE__, __LINE__, __func__)
+#define free(p)  do {                                                \
+        /*printf("%s:%d:%s:free(0x%lx)\n", __FILE__, __LINE__,          \
+               __func__, (unsigned long)p);*/                            \
+        if(p){                                                          \
+            free(p);                                                     \
+            p = NULL;                                                     \
+        }                                                                  \
+    } while (0)
+
+#endif
